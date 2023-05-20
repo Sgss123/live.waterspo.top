0c6e6da5b06e9350d738c9c97cb70f84443c3880
Sgss <103869422+Sgss123@users.noreply.github.com>
2023-05-20 23:58:41 +0000

    添加播放器

---
+++ controllers/player.js
@@ -0,0 +1,13 @@
+import Player from 'xgplayer/dist/simple_player';
+import volume from 'xgplayer/dist/controls/volume';
+import playbackRate from 'xgplayer/dist/controls/playbackRate';
+
+let player = new Player({
+  id: 'vs',
+  url: 'XXX.mp4',
+  controlPlugins: [
+    volume,
+    playbackRate
+  ],
+  playbackRate: [0.5, 0.75, 1, 1.5, 2] //传入倍速可选数组
+});
