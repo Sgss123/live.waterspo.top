f50a028dc69cc7a3b0f8bfbdc157766bdd2f3384
Sgss <103869422+Sgss123@users.noreply.github.com>
2023-05-21 00:06:59 +0000

    更改位置

--- routes/live.js
+++ routes/live.js
@@ -1,5 +1,19 @@
+import Player from 'xgplayer/dist/simple_player';
+import volume from 'xgplayer/dist/controls/volume';
+import playbackRate from 'xgplayer/dist/controls/playbackRate';
+
 var express = require('express');
 var router = express.Router();
+
+let player = new Player({
+  id: 'player',
+  url: '/videos/test.mp4',
+  controlPlugins: [
+    volume,
+    playbackRate
+  ],
+  playbackRate: [0.5, 0.75, 1, 1.5, 2] //传入倍速可选数组
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
